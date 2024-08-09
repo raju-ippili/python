ad68d6272ef6b19397d632b222dd9086c1164fed
Raju-Ippili <rajuippili09@gmail.com>
2024-08-09 22:39:24 +0530

    Add files via upload

---
+++ Staticvariable.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class Staticvariable
+{
+    static String name="raju";
+    public static void main(String args[])
+    {
+        Staticvariable s =new Staticvariable();
+        System.out.println(s.name);
+    }
+
+}
