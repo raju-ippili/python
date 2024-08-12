beff5128517b826072f79c9c940e301afe6fed67
Raju-Ippili <rajuippili09@gmail.com>
2024-08-12 14:41:40 +0530

    Add files via upload

---
+++ Cla1.java
@@ -0,0 +1,15 @@
+//Write a java program to read two values from commandline.
+import java.lang.*;
+import java.util.*;
+class Cla1
+{
+	public static void main(String args[])
+	{
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+
+		
+
+	}
+}
+//output: ramu jai
