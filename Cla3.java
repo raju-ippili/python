622c02ddc914ea46822e7419b37582bcc1673936
Raju-Ippili <rajuippili09@gmail.com>
2024-08-12 14:42:52 +0530

    Add files via upload

---
+++ Cla3.java
@@ -0,0 +1,16 @@
+//3.Write a java program to read values from commandline and also display count of input.
+import java.lang.*;
+import java.util.*;
+class Cla3
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<args.length;i++)
+		System.out.println(args[i]);
+		System.out.println(args.length);
+	}
+}
+//output
+//1:hari
+//2:hima
+//3
